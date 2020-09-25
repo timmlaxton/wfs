43a13370aec0dfcc91b4cdcab7fc5c2455141de5
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-25 09:37:56 +0100

    added routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ connectDB();
 
 app.get('/', (req, res) =>res.send('API Running'))
 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/todos', require('./routes/api/todos'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 
