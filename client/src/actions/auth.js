26ac95f2ed58c3288f74098247f16322b9973a98
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-28 11:37:50 +0100

    user can login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,9 +4,11 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL, 
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
  }  from './types';
- 
+
  import setAuthToken from '../utils/setAuthToken';
 
  export const loadUser = () => async dispatch => {
@@ -43,6 +45,7 @@ import {
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+      dispatch(loadUser())
     } catch (err) {
       const errors = err.response.data.errors
 
@@ -54,3 +57,33 @@ import {
       })
     }
  }
+
+ export const login = (email, password ) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({ email, password })
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch ({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser())
+  } catch (err) {
+    const errors = err.response.data.errors
+
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+    dispatch({
+      type: LOGIN_FAIL
+    })
+  }
+}
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, {Fragment, useState} from 'react';
-import {  Link  } from 'react-router-dom';
+import {  Link, Redirect  } from 'react-router-dom';
+import{ connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
 
   const [formData, setFormData] = useState({
     
@@ -17,9 +20,13 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-      console.log('SUCCESS')
+      login(email, password);
+    };
+
+    // Redirect when logged in
+    if(isAuthenticated) {
+      return <Redirect to= "/todos" />
     }
-  ;
 
   return  (
     
@@ -58,4 +65,13 @@ const Login = () => {
 )
 }
 
-export default Login
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated 
+})
+
+export default connect(mapStateToProps, { login })(Login)
